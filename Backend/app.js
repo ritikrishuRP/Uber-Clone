47f3b5a98dac7cdb97cb1209fbfdcd2159848366
rishu <rrishu212@gmail.com>
2025-02-24 08:35:15 +0530

    Captain Authentication & Route

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const app = express();
 const connectToDb = require('./db/db');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
 
 connectToDb();
 
@@ -20,6 +21,6 @@ app.use(cookieParser());
 // });
 
 app.use('/users', userRoutes);
-
+app.use('/captains', captainRoutes);
 
 module.exports = app;
--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blackListTokenModel = require('../models/blacklistToken.model');
+const captainModel = require('../models/captain.model');
 
 
 module.exports.authUser = async(req,res,next) => {
@@ -26,5 +28,35 @@ module.exports.authUser = async(req,res,next) => {
         return next();
     } catch (error) {
         return res.status(401).json({message: 'Unauthorized G'})
+    }
+}
+
+module.exports.authCaptain = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const token = req.cookies?.token || (authHeader && authHeader.split(' ')[1]);
+
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+
+
+
+    if (isBlacklisted) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id)
+        req.captain = captain;
+
+        return next()
+    } catch (err) {
+        console.log(err);
+
+        res.status(401).json({ message: 'Unauthorized' });
     }
 }
